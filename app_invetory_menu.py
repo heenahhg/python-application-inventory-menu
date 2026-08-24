8884567c5b463427fe13c14dffeb93a40a703a16
Hina Gohil <heena.hhg@gmail.com>
2026-08-24 15:13:29 +0200

    Refactor application selection and validation

--- app_invetory_menu.py
+++ app_invetory_menu.py
@@ -3,6 +3,9 @@
 # Developer: Hina Gohil
 # Version: 1.0
 # ==========================================
+
+import re
+
 applications =  [
     {
         "Name" : "Google Chrome",
@@ -10,6 +13,12 @@ applications =  [
         "Version" : "138",
         "Architecture" : "64-bit"
     },
+     {
+            "Name" : "Chrome Remote Control",
+            "Vendor" : "Google",
+            "Version" : "1.0.0",
+            "Architecture" : "64-bit"
+        },
     {
         "Name": "Microsoft Edge",
         "Vendor": "Microsoft",
@@ -33,7 +42,14 @@ def menu():
     print("3. Add Application")
     print("4. Delete Application")
     print("5. Update Application")
-    print("5. Exit")
+    print("6. Exit")
+
+
+def display_application(app):
+    print(f"Name            : {app['Name']}")
+    print(f"Vendor          : {app['Vendor']}")
+    print(f"Version         : {app['Version']}")
+    print(f"Architecture    : {app['Architecture']}")
 
 ## Display the application list
 def display_inventory(apps):
@@ -41,26 +57,32 @@ def display_inventory(apps):
 
     for number, app in enumerate(apps, start=1):
         print(f"\nApplication {number}")
-        print(f"Name            : {app['Name']}")
-        print(f"Vendor          : {app['Vendor']}")
-        print(f"Version         : {app['Version']}")
-        print(f"Architecture    : {app['Architecture']}")
+        display_application(app)
+      #  print(f"Name            : {app['Name']}")
+      #  print(f"Vendor          : {app['Vendor']}")
+      #  print(f"Version         : {app['Version']}")
+      #  print(f"Architecture    : {app['Architecture']}")
 
 ##searching for the application
 
 def search_application(apps, app_name):
     print("\nSearching for the application...........")
+    matches = []
+
     for app in apps:
-        if app["Name"] == app_name:
-            print("\nApplication Found.")
-            print(f"Name            : {app['Name']}")
-            print(f"Vendor          : {app['Vendor']}")
-            print(f"Version         : {app['Version']}")
-            print(f"Architecture    : {app['Architecture']}")
-            return
+        if app_name.lower() in app["Name"].lower():
+            matches.append(app)
 
-    print(f"\n{app_name} was not found.")
+    if len(matches) == 0:
+        print("Application not found.")
+        return
 
+    print(f"\nApplications found: {len(matches)}")
+
+    for number, app in enumerate(matches, start=1):
+        print(f"\nApplication {number}")
+        display_application(app)
+         
 ## Adding the application
 #new_application = {
 #    "Name": "Adobe Reader",
@@ -69,36 +91,102 @@ def search_application(apps, app_name):
 #    "Architecture": "64-bit"
 #}
 
+def select_application(apps, app_name):
+    matches = []
+
+    for app in apps:
+        if app_name.lower() in app["Name"].lower():
+            matches.append(app)
+
+    if len(matches) == 0:
+        print("Application not found.")
+        return None
+    elif len(matches) == 1:
+        return matches[0]
+    elif len(matches) > 1:
+        print("\nMultiple applications found:", len(matches))
+        
+        for number, app in enumerate(matches, start=1):
+                print(f"Application {number}: {app['Name']}")
+
+        selected = input("Enter the number of the application:")
+                
+        try:
+            selected = int(selected)
+        except ValueError:
+            print("Invalid application number.")
+            return None
+                   
+        if selected < 1 or selected > len(matches):
+            print("Invalid application number.")
+            return None
+                               
+        app = matches[selected - 1]
+                   
+        print("Selected application:", app["Name"])
+        return app    
+       
+def application_exists(apps, app_name):
+    for app in apps:
+        if app["Name"].lower() == app_name.lower():
+            return True
+
+    return False
 
 def add_application(apps, new_app):
     print("\nAdding the new application...")
-    for app in apps:
-        if app["Name"] == new_app["Name"]:
-            print("\nApplication already exists.")
-            return
-
     apps.append(new_app)
     print(f"\nApplication added successfully.")
 
+
 def delete_application(apps, app_name):
-    print("\nSearching for the application...")
-    for app in apps:
-        if app["Name"] == app_name:
-            apps.remove(app)
-            print(f"{app_name} deleted successfully.")
-            return
-    print("Application not found.")
+    app = select_application(apps, app_name)
+
+    if app is None:
+        return
+    apps.remove(app)
+    print(f"{app['Name']} deleted successfully.")
+
+def validate_version(version):
+    return bool(re.fullmatch(r"\d+(\.\d+)*", version))
 
 def update_app (apps, app_name):
-    print("\nSearching for the application...")
-    for app in apps:
-        if app["Name"] == app_name:
-            new_version = input("Enter New Version:")
+    
+    app = select_application(apps, app_name)
+
+    if app is None:
+        return
+    print("1. Version")
+    print("2. Vendor")
+    print("3. Architecture")
+    choice = input("Enter your choice: ")
+    print(f"You selected: {choice}")
+
+    if choice == "1":
+            new_version = input("Enter New Version:").strip()
+            if not validate_version(new_version):
+                    print("Invalid version format.")
+                    return
             app["Version"] = new_version
-            
-            print(f"{app_name} updated successfully.")
+            print(f"{app['Name']} updated successfully.")
+    elif choice == "2":
+            new_vendor = input("Enter New Vendor:").strip()
+            if new_vendor == "":
+                print("Application vendor cannot be empty.")
+                return
+            app["Vendor"] = new_vendor
+            print(f"{app['Name']} updated successfully.")
+    elif choice == "3":
+            new_arc = input("Enter New Architecture:").strip()
+            if new_arc == "":
+                 print("Application architecture cannot be empty.")
+                 return
+            app["Architecture"] = new_arc
+            print(f"{app['Name']} updated successfully.")
+    else:
+            print("Invalid Choice.")
             return
-    print("Application not found.")
+
 
 while True:
 
@@ -110,15 +198,34 @@ while True:
     if choice == "1":
         #print(type(choice))
         display_inventory(applications)
+        
     elif choice ==  "2":
-        app_name = input("Enter application name to search:")
+        app_name = input("Enter application name to search:").strip()
+        if app_name == "":
+            print("Application name cannot be empty.")
+            continue
         #print(f"{app_name}")
         search_application(applications, app_name)
     elif choice == "3":
-        name = input("Enter application name:")
-        vendor = input("Enter vendor       :")
-        version = input("Enter version     :")
-        architecture = input("Enter architecture   :")
+        name = input("Enter application name:").strip()
+        if name == "":
+            print("Application name cannot be empty.")
+            continue
+        if application_exists(applications, name):
+            print("Application already exists.")
+            continue
+        vendor = input("Enter vendor       :").strip()
+        if vendor == "":
+            print("Application vendor cannot be empty.")
+            continue
+        version = input("Enter version     :").strip()
+        if not validate_version(version): 
+            print("Invalid version format.")
+            continue
+        architecture = input("Enter architecture   :").strip()
+        if architecture == "":
+            print("Architecture cannot be empty.")
+            continue
 
         new_app = {
             "Name" : name, 
@@ -130,11 +237,14 @@ while True:
         add_application(applications, new_app)
 
     elif choice == "4" :
-        del_app = input("Enter application name to delete:")
+        del_app = input("Enter application name to delete:").strip()
         delete_application(applications, del_app)
     elif choice == "5" :
-            upd_app = input("Enter application name to update:")
-            update_app(applications, upd_app)
+            app_name = input("Enter application name to update:").strip()
+            if app_name == "":
+                print("Application name cannot be empty.")
+                continue
+            update_app(applications, app_name)
 
     elif choice == "6":
         print("Exiting...")
